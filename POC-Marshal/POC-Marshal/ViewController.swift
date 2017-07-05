b956dfc956bec4a9aaf14afcd39aca7d81dbd5a0
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2017-07-05 12:42:35 +0530

    Created project with Marshal

---
+++ POC-Marshal/POC-Marshal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POC-Marshal
+//
+//  Created by Nitesh Meshram on 7/5/17.
+//  Copyright © 2017 V2Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
