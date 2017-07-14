e45401bca2772bdeb3e6afdfbe93b7256d24bf98
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2017-07-14 17:53:24 +0530

    Resolved issues

--- POC-Marshal/POC-Marshal/ViewController.swift
+++ POC-Marshal/POC-Marshal/ViewController.swift
@@ -15,16 +15,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        do {
-            let obj = personsJSON()
-            let context = DeserializationContext()
-            
-            let people: [Person] = try obj.value(for: "people", inContext: context)
-            
-        } catch {
-            
-        }
+//        do {
+//            let obj = personsJSON()
+//            let context = DeserializationContext()
+//            
+//            let people: [Person] = try obj.value(for: "people", inContext: context)
+//            
+//        } catch {
+//            
+//        }
         
+        let obj = personsJSON()
+        let context = DeserializationContext()
+        
+        let people: [Person] = try! obj.value(for: "people", inContext: context)
 
     }
 
@@ -52,10 +56,48 @@ private struct Person {
     var address: Address?
 }
 
+extension Person: UnmarshalUpdatingWithContext {
+    mutating func update(object: MarshaledObject, inContext context: DeserializationContext) throws {
+        firstName = try object.value(for: "first")
+        lastName = try object.value(for: "last")
+        score = try object.value(for: "score")
+        address = try object.value(for: "address", inContext: context)
+    }
+}
+
+extension Person: UnmarshalingWithContext {
+    static func value(from object: MarshaledObject, inContext context: DeserializationContext) throws -> Person {
+        var person = context.newPerson()
+        try person.update(object: object, inContext: context)
+        return person
+    }
+}
+
+
 private struct Address {
     var street: String!
     var city: String!
 }
+
+
+extension Address: UnmarshalUpdating {
+    mutating func update(with object: MarshaledObject) throws {
+        street = try object.value(for: "street")
+        city = try object.value(for: "city")
+    }
+}
+
+extension Address: UnmarshalingWithContext {
+    static func value(from object: MarshaledObject, inContext context: DeserializationContext) throws -> Address {
+        var address = context.newAddress()
+        try address.update(with: object)
+        return address
+    }
+}
+
+
+
+
 private class DeserializationContext {
     func newPerson() -> Person {
         return Person()
@@ -65,7 +107,4 @@ private class DeserializationContext {
         return Address()
     }
     
-    //    func newAgedPerson() -> AgedPerson {
-    //        return AgedPerson()
-    //    }
 }
